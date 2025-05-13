91588ca47c50b3af75d30cf6a4c29c62803600aa
Yuyuto000 <yukkurigame0101@gmail.com>
2025-05-13 17:16:44 +0900

    gcc.el

---
+++ gcc.el
@@ -0,0 +1,20 @@
+package com.hello
+import os
+
+msg "hello"
+function(name=add,a,b){
+	i = a + b
+	return i
+}
+file.open(c://file,dom=.txt){
+	parm1 = input("please type me")
+	parm2 = input("please type me")
+	add(parm1,parm2,return=i)
+	if(name == :num:){
+		msg "it is number for "+{i}!
+	} else {
+		msg "it is number!"
+	}
+file.edit("input value="{i}
+}
+exit;
